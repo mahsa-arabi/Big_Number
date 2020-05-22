c4edbf427dc10ff06085d9b30065e467fbd7b0f6
mahsa-arabi <mahsaarabi26@yahoo.com>
2020-05-22 04:27:39 -0700

    add some tests to the main

--- main.cpp
+++ main.cpp
@@ -2,30 +2,25 @@
 #include <cstdint>
 
 #include "BigNumber.h"
-
+#include "MyBigNumber.h"
 using namespace std;
 
 int main() {
     cout << boolalpha;
-
-//    for(unsigned char i{0}; i <= 127; ++i){
-//        std::cout << (int) i << '\t' << i << std::endl;
-//    }
-    //std::cout << x << std::endl;
-//    BigNumber x{-37};
-//    string str = "-0000";
-//    BigNumber y{str};
-//    cout << y << endl;
-//    cout << "Enter a number: ";
-//    cin >> x;
-//    x = -y;
-//    BigNumber z = -y;
-//    cout << (x != y) << endl;
-    BigNumber myBig1 = -17319;
-    BigNumber myBig2 = "0000173";
-    //cout << BigNumber::unsignedLessOrEqual(myBig1, myBig2) << endl;
-    //BigNumber a = BigNumber::unsignedSubtract(myBig1, myBig2);
-    cout << (myBig1>>3) << "\t" << (myBig1>>3).getSign() << endl;
+    BigNumber x{-37};
+    string str = "-0000";
+    BigNumber y{str};
+    cout << y << endl;
+   // cout << "Enter a number: ";
+   //cin >> x;
+    x = -y;
+    BigNumber z = -y;
+    cout << (x != y) << endl;
+  BigNumber myBig1 = "-17319";
+  BigNumber myBig2 = "000870";
+ // cout << BigNumber::unsignedLessOrEqual(myBig1, myBig2) << endl;
+  //BigNumber a = BigNumber::unsignedSubtract(myBig1, myBig2);
+    cout << (myBig2>>1) << "\t" << (myBig1>>3).getSign() << endl;
 //    try{
 //        cout << BigNumber::unsignedSubtract(myBig1, myBig2) << endl;
 //    }
